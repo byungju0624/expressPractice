55c8e9e60ebff9fd6a1bd073a03df8e9aabd7a6a
byungju0624 <byungju0624@gmail.com>
2021-09-29 12:34:43 +0900

    jest study in express

---
+++ meadowlark/site/meadowlark-04.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const handlers = require("./lib/handler");
+const app = express();
+const port = 3001;
+
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
+app.get("/", handlers.home);
+app.get("/about", handlers.about);
+app.use(handlers.notFound);
+app.use(handlers.serverError);
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(
+      `express started on http://localhost:${port}` +
+        "; press Ctrl-C to terminate."
+    );
+  });
+} else {
+  module.exports = app;
+}
