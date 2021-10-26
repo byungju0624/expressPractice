aa2430c8d2ff223bdad6d99bdcda4db513909e19
byungju0624 <byungju0624@gmail.com>
2021-10-26 12:50:30 +0900

    cookie and session study in express

--- meadowlark/site/lib/handler.js
+++ meadowlark/site/lib/handler.js
@@ -1,46 +1,75 @@
-const { query } = require("express");
-const fortune = require("./fortune");
-exports.home = (req, res) => res.render("home");
-exports.about = (req, res) =>
-  res.render("about", { fortune: fortune.getFortune() });
-exports.notFound = (req, res) => res.render("404");
-/* eslint-disable no-unused-vars */
-exports.serverError = (err, req, res, next) => res.render("500");
-exports.sectionTest = (req, res) => res.render("section-test");
+// slightly modified version of the official W3C HTML5 email regex:
+// https://html.spec.whatwg.org/multipage/forms.html#valid-e-mail-address
+const VALID_EMAIL_REGEX = new RegExp(
+  "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@" +
+    "[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?" +
+    "(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$"
+);
 
-exports.newletterSignup = (req, res) => {
+// fake "newsletter signup" interface
+class NewsletterSignup {
+  constructor({ name, email }) {
+    this.name = name;
+    this.email = email;
+  }
+  async save() {
+    // here's where we would do the work of saving to a database
+    // since this method is async, it will return a promise, and
+    // since we're not throwing any errors, the promise will
+    // resolve successfully
+  }
+}
+
+exports.home = (req, res) => res.render("home");
+
+exports.newsletterSignup = (req, res) => {
+  // we will learn about CSRF later...for now, we just
+  // provide a dummy value
   res.render("newsletter-signup", { csrf: "CSRF token goes here" });
 };
-exports.newletterSignupProcess = (req, res) => {
-  console.log("Form(from querystring): " + req, query.form);
-  console.log(`CSRF token (from hidden form field): ${req.body._csrf}`);
-  console.log(`Name (from visible form field): ${req.body.name}`);
-  console.logO(`Email (from visible form field): ${req.body.email}`);
-  res.redirect(303, "/newsletter-signup-thank-you");
+exports.newsletterSignupProcess = (req, res) => {
+  const name = req.body.name || "",
+    email = req.body.email || "";
+  // input validation
+  if (!VALID_EMAIL_REGEX.test(email)) {
+    req.session.flash = {
+      type: "danger",
+      intro: "Validation error!",
+      message: "The email address you entered was not valid.",
+    };
+    return res.redirect(303, "/newsletter-signup");
+  }
+  // NewsletterSignup is an example of an object you might create; since
+  // every implementation will vary, it is up to you to write these
+  // project-specific interfaces.  This simply shows how a typical
+  // Express implementation might look in your project.
+  new NewsletterSignup({ name, email })
+    .save()
+    .then(() => {
+      req.session.flash = {
+        type: "success",
+        intro: "Thank you!",
+        message: "You have now been signed up for the newsletter.",
+      };
+      return res.redirect(303, "/newsletter-archive");
+    })
+    .catch((err) => {
+      req.session.flash = {
+        type: "danger",
+        intro: "Database error!",
+        message: "There was a database error; please try again later.",
+      };
+      return res.redirect(303, "/newsletter-archive");
+    });
 };
-exports.newsletterSignupThankYou = (req, res) => {
+exports.newsletterSignupThankYou = (req, res) =>
   res.render("newsletter-signup-thank-you");
-};
+exports.newsletterArchive = (req, res) => res.render("newsletter-archive");
 
-exports.newsletter = (req, res) => {
-  res.render("newsletter", { csrf: "CSRF token goes here" });
-};
-exports.api = (req, res) => {
-  console.log(`CSRF token (from hidden form field): ${req.body._csrf}`);
-  console.log(`Name (from visible form field): ${req.body.name}`);
-  console.log(`Email (from visible form field): ${req.body.email}`);
-  res.send({ result: "success" });
-};
+exports.notFound = (req, res) => res.render("404");
+
+// Express recognizes the error handler by way of its four
+// argumetns, so we have to disable ESLint's no-unused-vars rule
+/* eslint-disable no-unused-vars */
+exports.serverError = (err, req, res, next) => res.render("500");
 /* eslint-enable no-unused-vars */
-exports.vacationPhotoContest = (req, res) => {
-  const now = new Date();
-  res.render("contest/vacation-photo", {
-    year: now.getFullYear(),
-    month: now.getMonth(),
-  });
-};
-exports.vacationPhotoContestProcess = (req, res, fields, files) => {
-  console.log(`field data: ${fields}`);
-  console.log(`files: ${files}`);
-  res.redirect(303, "/contest/vacation-photo-thank-you");
-};
