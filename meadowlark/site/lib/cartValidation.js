d209298ef8883aa012c8d5ce40183281458b34c4
byungju0624 <byungju0624@gmail.com>
2021-10-26 15:24:59 +0900

    middleware study in express

---
+++ meadowlark/site/lib/cartValidation.js
@@ -0,0 +1,29 @@
+module.exports = {
+  resetValidation(req, res, next) {
+    const { cart } = req.session;
+    if (cart) cart.warnings = cart.errors = [];
+    next();
+  },
+  checkWaivers(req, res, next) {
+    const { cart } = req.session;
+    if (!cart) return next();
+    if (cart.items.some((item) => item.product.requiresWaiver)) {
+      cart.warnings.push(
+        "One or more of your selected " + "tours requires a waiver."
+      );
+    }
+    next();
+  },
+  checkGuestCounts(req, res, next) {
+    const { cart } = req.session;
+    if (!cart) return next();
+    if (cart.items.some((item) => item.guestss > item.product.maxGuests)) {
+      cart.errors.push(
+        "One or more of yout selected tours " +
+          "cannot accommodate the number of guests you " +
+          "have selected."
+      );
+    }
+    next();
+  },
+};
